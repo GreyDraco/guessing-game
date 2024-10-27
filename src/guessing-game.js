93208538e3bffc79cc9a56998a29f49c4625ddcc
GreyDraco <ancientgreydragon@gmail.com>
2024-10-27 22:32:47 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        if (min >= max) {
+            throw new Error("Minimum should be less than maximum.");
+        }
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.round((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess;
     }
 
     greater() {
-
+        this.min = this.currentGuess;
     }
 }
 
